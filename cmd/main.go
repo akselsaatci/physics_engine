4af2afb159e150b11d7c40b6b2e961ff08bd1146
akselsaatci <100696686+akselsaatci@users.noreply.github.com>
2024-05-07 10:12:44 +0200

    refactoring

--- cmd/main.go
+++ cmd/main.go
@@ -4,8 +4,8 @@ import (
 	"image/color"
 	"log"
 
+    "github.com/akselsaatci/pkg/objects/CircleObject"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 type Game struct {
@@ -18,52 +18,35 @@ const gravity_y float32 = 1
 // TODO calculate this later
 const dt float32 = 1.0 / 60
 
-type CircleObject struct {
-	pos_x_old      float32
-	pos_y_old      float32
-	pos_x          float32
-	pos_y          float32
-	width          float32
-	color          color.Color
-	acceleration_x float32
-	acceleration_y float32
+type CustomColor struct {
+	R uint8
+	G uint8
+	B uint8
+	A uint8
 }
 
-func (object *CircleObject) Update() {
-	vel_x := object.pos_x - object.pos_x_old
-	vel_y := object.pos_y - object.pos_y_old
+func (c CustomColor) RGBA() (uint32, uint32, uint32, uint32) {
+	// convert components to float32 in the range [0.0, 1.0]
+	r32f := float32(c.R) / float32(255.0)
+	g32f := float32(c.G) / float32(255.0)
+	b32f := float32(c.B) / float32(255.0)
+	a32f := float32(c.A) / float32(255.0)
 
-	object.pos_x_old = object.pos_x
-	object.pos_y_old = object.pos_y
+	// perform alpha-premultiplication
+	r32f = r32f * a32f
+	g32f = g32f * a32f
+	b32f = b32f * a32f
 
-	object.pos_x = object.pos_x + vel_x + object.acceleration_x*dt*dt
-	object.pos_y = object.pos_y + vel_y + object.acceleration_y*dt*dt
+	// convert back to 16 bit
+	r16 := uint16(r32f * 65535.0)
+	g16 := uint16(g32f * 65535.0)
+	b16 := uint16(b32f * 65535.0)
+	a16 := uint16(a32f * 65535.0)
 
-	object.acceleration_x = 0
-	object.acceleration_y = 0
+	// return as 32 bit (without upscaling)
+	return uint32(r16), uint32(g16), uint32(b16), uint32(a16)
 }
-func (object *CircleObject) accelerate(acc_x, acc_y float32) {
-	object.acceleration_x = acc_x
-	object.acceleration_y = acc_y
 
-}
-
-func (object *CircleObject) Draw(screen *ebiten.Image) {
-	vector.DrawFilledCircle(screen, object.pos_x, object.pos_y, object.width, object.color, false)
-}
-
-func MakeNewCircleObject(loc_x, loc_y, width float32, color color.Color) *CircleObject {
-	return &CircleObject{
-		pos_x_old:      loc_x,
-		pos_y_old:      loc_y,
-		pos_x:          loc_x,
-		pos_y:          loc_y,
-		width:          width,
-		color:          color,
-		acceleration_x: 0,
-		acceleration_y: 0,
-	}
-}
 func (g *Game) Update() error {
 	for _, circle := range g.Circles {
 		circle.accelerate(0, gravity_y)
@@ -78,6 +61,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 
 	}
 }
+
 // TODO should look what is this
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return 320, 240
@@ -87,6 +71,8 @@ func main() {
 	ebiten.SetWindowSize(1412, 832)
 	ebiten.SetWindowTitle("Hello, World!")
 
+	r := CustomColor{255, 0, 0, 255}
+    
 	game := &Game{
 		Circles: []*CircleObject{
 			MakeNewCircleObject(160, 120, 5, color.White),
